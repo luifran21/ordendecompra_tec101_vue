8c300420f27db08cbb8b5666bd324b2b266241a8
Luis-Rivas <71407850+Luis-Rivas@users.noreply.github.com>
2024-01-11 11:41:05 -0600

    Update main.js
    add url base del backend

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ import * as Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
+window.axios = axios;
+window.axios.defaults.baseURL="http://localhost:8000/api"
+
+
 const app = createApp(App)
 
 app.use(router)
