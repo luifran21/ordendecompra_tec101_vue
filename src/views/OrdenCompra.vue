8f9ac007290ff1ea4fc856424ace7ed83215ebeb
Luis-Rivas <71407850+Luis-Rivas@users.noreply.github.com>
2024-01-11 06:47:46 -0600

    commit inicial

---
+++ src/views/OrdenCompra.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+<template>
+    <h1>Hola Mundo</h1>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+  mounted() {
+    console.log(`The initial count is ${this.count}.`)
+  }
+}
+</script>
